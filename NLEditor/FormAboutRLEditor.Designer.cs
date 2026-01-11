fb0a9789f7c95b5d95ab4800d71f6e34374d7cb9
Willicious <williciousmedia@gmail.com>
2026-01-11 16:25:57 +0000

    Added "Whats New" text as a picture (users may not have the hobo font installed)

--- NLEditor/FormAboutRLEditor.Designer.cs
+++ NLEditor/FormAboutRLEditor.Designer.cs
@@ -31,9 +31,6 @@ namespace RLEditor
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAboutRLEditor));
-            this.lblWhatsNew = new System.Windows.Forms.Label();
-            this.pictureFloater = new System.Windows.Forms.PictureBox();
-            this.pictureClimber = new System.Windows.Forms.PictureBox();
             this.check_ShowThisWindow = new System.Windows.Forms.CheckBox();
             this.lblPreviousUpdates = new System.Windows.Forms.Label();
             this.richTextBox_PreviousUpdates = new System.Windows.Forms.RichTextBox();
@@ -45,44 +42,15 @@ namespace RLEditor
             this.lblLFCommunity = new System.Windows.Forms.Label();
             this.linkLF = new System.Windows.Forms.LinkLabel();
             this.lblBasedOn = new System.Windows.Forms.Label();
+            this.pictureWhatsNew = new System.Windows.Forms.PictureBox();
+            this.pictureClimber = new System.Windows.Forms.PictureBox();
+            this.pictureFloater = new System.Windows.Forms.PictureBox();
             this.picturePadding = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureFloater)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWhatsNew)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureClimber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFloater)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picturePadding)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblWhatsNew
-            // 
-            this.lblWhatsNew.AutoSize = true;
-            this.lblWhatsNew.Font = new System.Drawing.Font("Hobo Std", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWhatsNew.Location = new System.Drawing.Point(555, 16);
-            this.lblWhatsNew.Name = "lblWhatsNew";
-            this.lblWhatsNew.Size = new System.Drawing.Size(222, 51);
-            this.lblWhatsNew.TabIndex = 0;
-            this.lblWhatsNew.Text = "What\'s New";
-            // 
-            // pictureFloater
-            // 
-            this.pictureFloater.Image = global::RLEditor.Properties.Resources.IntroFloater;
-            this.pictureFloater.Location = new System.Drawing.Point(31, 16);
-            this.pictureFloater.Name = "pictureFloater";
-            this.pictureFloater.Size = new System.Drawing.Size(156, 220);
-            this.pictureFloater.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureFloater.TabIndex = 2;
-            this.pictureFloater.TabStop = false;
-            // 
-            // pictureClimber
-            // 
-            this.pictureClimber.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureClimber.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureClimber.Image = global::RLEditor.Properties.Resources.IntroClimber;
-            this.pictureClimber.InitialImage = null;
-            this.pictureClimber.Location = new System.Drawing.Point(1170, 82);
-            this.pictureClimber.Name = "pictureClimber";
-            this.pictureClimber.Size = new System.Drawing.Size(96, 179);
-            this.pictureClimber.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureClimber.TabIndex = 3;
-            this.pictureClimber.TabStop = false;
             // 
             // check_ShowThisWindow
             // 
@@ -134,7 +102,7 @@ namespace RLEditor
             this.lblRetroLemminiEditor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRetroLemminiEditor.Location = new System.Drawing.Point(521, 585);
             this.lblRetroLemminiEditor.Name = "lblRetroLemminiEditor";
-            this.lblRetroLemminiEditor.Size = new System.Drawing.Size(302, 25);
+            this.lblRetroLemminiEditor.Size = new System.Drawing.Size(301, 25);
             this.lblRetroLemminiEditor.TabIndex = 10;
             this.lblRetroLemminiEditor.Text = "RetroLemmini Editor (Version)";
             // 
@@ -196,6 +164,39 @@ namespace RLEditor
             this.lblBasedOn.TabIndex = 18;
             this.lblBasedOn.Text = "Based on the NeoLemmix Editor by Stephan Neupert (Nepster) and Namida Verasche";
             // 
+            // pictureWhatsNew
+            // 
+            this.pictureWhatsNew.Image = global::RLEditor.Properties.Resources.WhatsNew;
+            this.pictureWhatsNew.Location = new System.Drawing.Point(353, 16);
+            this.pictureWhatsNew.Name = "pictureWhatsNew";
+            this.pictureWhatsNew.Size = new System.Drawing.Size(642, 57);
+            this.pictureWhatsNew.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureWhatsNew.TabIndex = 19;
+            this.pictureWhatsNew.TabStop = false;
+            // 
+            // pictureClimber
+            // 
+            this.pictureClimber.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureClimber.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureClimber.Image = global::RLEditor.Properties.Resources.IntroClimber;
+            this.pictureClimber.InitialImage = null;
+            this.pictureClimber.Location = new System.Drawing.Point(1170, 82);
+            this.pictureClimber.Name = "pictureClimber";
+            this.pictureClimber.Size = new System.Drawing.Size(96, 179);
+            this.pictureClimber.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureClimber.TabIndex = 3;
+            this.pictureClimber.TabStop = false;
+            // 
+            // pictureFloater
+            // 
+            this.pictureFloater.Image = global::RLEditor.Properties.Resources.IntroFloater;
+            this.pictureFloater.Location = new System.Drawing.Point(31, 16);
+            this.pictureFloater.Name = "pictureFloater";
+            this.pictureFloater.Size = new System.Drawing.Size(156, 220);
+            this.pictureFloater.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureFloater.TabIndex = 2;
+            this.pictureFloater.TabStop = false;
+            // 
             // picturePadding
             // 
             this.picturePadding.BackColor = System.Drawing.SystemColors.ControlDark;
@@ -215,6 +216,7 @@ namespace RLEditor
             this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(1300, 811);
+            this.Controls.Add(this.pictureWhatsNew);
             this.Controls.Add(this.lblBasedOn);
             this.Controls.Add(this.pictureClimber);
             this.Controls.Add(this.linkLF);
@@ -227,7 +229,6 @@ namespace RLEditor
             this.Controls.Add(this.lblPreviousUpdates);
             this.Controls.Add(this.check_ShowThisWindow);
             this.Controls.Add(this.pictureFloater);
-            this.Controls.Add(this.lblWhatsNew);
             this.Controls.Add(this.lblDMA);
             this.Controls.Add(this.picturePadding);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -239,8 +240,9 @@ namespace RLEditor
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About RetroLemmini Editor";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormAboutRLEditor_KeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureFloater)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWhatsNew)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureClimber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFloater)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picturePadding)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -248,8 +250,6 @@ namespace RLEditor
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblWhatsNew;
         private PictureBox pictureFloater;
         private PictureBox pictureClimber;
         private CheckBox check_ShowThisWindow;
@@ -264,5 +264,6 @@ namespace RLEditor
         private LinkLabel linkLF;
         private Label lblBasedOn;
         private PictureBox picturePadding;
+        private PictureBox pictureWhatsNew;
     }
 }
