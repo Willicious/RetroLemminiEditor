c5d91d71a3db3f2a27065482a56c416e514f9dc8
Willicious <williciousmedia@gmail.com>
2026-03-16 14:44:11 +0000

    === Version 1.4 ===

--- RLEditor/FormAboutRLEditor.cs
+++ RLEditor/FormAboutRLEditor.cs
@@ -106,32 +106,26 @@ namespace RLEditor
 
             WriteBoldText(richTextBox, $"================ Version {C.Version} Updates ================\n");
 
-            WriteBoldText(richTextBox, "\n• Templates\n");
-            richTextBox.AppendText(" • It's now possible to create, save and load level templates.\n");
-            richTextBox.AppendText(" • From the Template Loader, choose a template to use as default. This template will then be loaded when opening the Editor or creating a new level.\n");
-            richTextBox.AppendText(" • The Template Loader is shown on startup by default. This can be toggled on/off at any time.\n");
+            WriteBoldText(richTextBox, "\n• Direct Drop (Maximum Exit Physics) support added\n");
+            richTextBox.AppendText(" • It's now possible to set Direct Drop (Maximum Exit Physics) on a per-level basis. With this property activated, lemmings will always interact with the Exit's trigger (regardless of state/action), meaning that lems can exit in midair and from otherwise unsurvivable drop heights (aka Direct Drop), and will prioritise an Exit's trigger over any other object wherever they overlap.\n");
 
-            WriteBoldText(richTextBox, "\n• Crop Level Width/Height\n");
-            richTextBox.AppendText(" • Added a new Crop rectangle which makes it much quicker and easier to adjust level width and height to fit the layout.\n");
-            richTextBox.AppendText(" • This has full hotkey support (X to toggle the Crop rectangle, Enter to apply, Esc to cancel.\n");
-            richTextBox.AppendText(" • Note that some Editor features (such as dragging pieces) become unavailable when the Crop rectangle is active.\n");
+            WriteBoldText(richTextBox, "\n• Control Hints\n");
+            richTextBox.AppendText(" • When the mouse is hovered over a control, information about that control is now displayed in the status bar. This can be toggled on/off in Settings.\n");
 
-            WriteBoldText(richTextBox, "\n• UI - Level Arranger\n");
-            richTextBox.AppendText(" • Increased minimum zoom to -3.\n");
-            richTextBox.AppendText(" • Improved/fixed layout of corner text (in both docked and windowed Level Arranger).\n");
+            WriteBoldText(richTextBox, "\n• Level Pack Compiler - Automatic Updates/UI\n");
+            richTextBox.AppendText(" • The Level Pack Compiler now silently checks for updates every 30 days and automatically downloads the latest version.\n");
 
-            WriteBoldText(richTextBox, "\n• UI - Piece Browser - Auto-replace selected pieces\n");
-            richTextBox.AppendText(" • Pressing [Ctrl] or [Shift] whilst clicking-to-add a piece from the Piece Browser will directly replace any currently-selected piece in the Level Arranger. Note that this only works if a single piece is selected.\n");
-            richTextBox.AppendText(" • Pressing [Alt] whilst clicking-to-add a piece from the Piece Browser will add that piece to the same X/Y co-ordinates as any currently-selected piece in the Level Arranger. Again, this only works if a single piece is selected.\n");
+            WriteBoldText(richTextBox, "\n• Style Manager/UI\n");
+            richTextBox.AppendText(" • Added 'Remove' button so that styles can be removed from the list.\n");
+            richTextBox.AppendText(" • Bugfix - 'Sort Alphabetically' becomes available only when multiple items are selected.\n");
 
-            WriteBoldText(richTextBox, "\n• Hotkeys\n");
-            richTextBox.AppendText(" • Removed support for Classic hotkeys (not relevant in this version of the Editor).\n");
+            WriteBoldText(richTextBox, "\n• Skillset\n");
+            richTextBox.AppendText(" • Added support for 'Infinity' skill amount (applies when the numeric control is set to 100).\n");
 
-            WriteBoldText(richTextBox, "\n• Bugfixes/UI");
-            richTextBox.AppendText(" • The selection rectangle is now drawn around the full object for OWWs.\n");
-            richTextBox.AppendText(" • Piece selection is now preserved on Undo/Redo.\n");
-            richTextBox.AppendText(" • Dropdown lists now show more items when expanded.\n");
-            richTextBox.AppendText(" • Grid is now drawn to its own layer.\n");
+            WriteBoldText(richTextBox, "\n• Bugfixes/UI\n");
+            richTextBox.AppendText(" • Corrected Y-Position of hatch spawn point.\n");
+            richTextBox.AppendText(" • 'Show Missing Pieces' menu item is now only present when relevant (i.e. when the level contains missing pieces).\n");
+            richTextBox.AppendText(" • Transformations (rotate/invert/flip) no longer apply for One-Way-Walls (matches RetroLemmini 2.9 behaviour).\n");
 
             // Version 1.1 features
             WriteBoldText(richTextBox, $"\n\n================ Previous Updates ================\n");
@@ -142,14 +136,30 @@ namespace RLEditor
             WriteBoldText(richTextBox, "\n• Front-panel control for all level features\n");
             richTextBox.AppendText(" • Name your level, resize it, add a skillset, add pieces, and much more, all from the various tabs and controls on the main dashboard.\n");
 
+            WriteBoldText(richTextBox, "\n• Templates\n");
+            richTextBox.AppendText(" • It's now possible to create, save and load level templates.\n");
+            richTextBox.AppendText(" • From the Template Loader, choose a template to use as default. This template will then be loaded when opening the Editor or creating a new level.\n");
+            richTextBox.AppendText(" • The Template Loader is shown on startup by default. This can be toggled on/off at any time.\n");
+
+            WriteBoldText(richTextBox, "\n• Crop Level Width/Height\n");
+            richTextBox.AppendText(" • Added a new Crop rectangle which makes it much quicker and easier to adjust level width and height to fit the layout.\n");
+            richTextBox.AppendText(" • This has full hotkey support (X to toggle the Crop rectangle, Enter to apply, Esc to cancel.\n");
+            richTextBox.AppendText(" • Note that some Editor features (such as dragging pieces) become unavailable when the Crop rectangle is active.\n");
+
             WriteBoldText(richTextBox, "\n• Default Author Name\n");
             richTextBox.AppendText(" • Added a setting which automatically applies a default author name when a new level is created\n");
 
             WriteBoldText(richTextBox, "\n• Rulers\n");
             richTextBox.AppendText(" • Use rulers to measure builder bridges, basher tunnels, fall distance, and more.\n");
 
+            WriteBoldText(richTextBox, "\n• UI - Level Arranger\n");
+            richTextBox.AppendText(" • Increased minimum zoom to -3.\n");
+            richTextBox.AppendText(" • Improved/fixed layout of corner text (in both docked and windowed Level Arranger).\n");
+
             WriteBoldText(richTextBox, "\n• Piece Browser\n");
             richTextBox.AppendText(" • Added a 'Random' button to the Piece Browser which, when clicked, randomized the piece style selection. It's possible to specify which styles are Randomized in the Style Manager; if no styles are specified, the entire list is randomized\n");
+            richTextBox.AppendText(" • Pressing [Ctrl] or [Shift] whilst clicking-to-add a piece from the Piece Browser will directly replace any currently-selected piece in the Level Arranger. Note that this only works if a single piece is selected.\n");
+            richTextBox.AppendText(" • Pressing [Alt] whilst clicking-to-add a piece from the Piece Browser will add that piece to the same X/Y co-ordinates as any currently-selected piece in the Level Arranger. Again, this only works if a single piece is selected.\n");
 
             WriteBoldText(richTextBox, "\n• Level Pack Compiler\n");
             richTextBox.AppendText(" • Compile your levels into a level pack for RetroLemmini.\n");
@@ -168,6 +178,9 @@ namespace RLEditor
             richTextBox.AppendText(" • Added support for OWW Up/Down (available in RetroLemmini 2.8 onwards)\n");
             richTextBox.AppendText(" • Note that it's no longer possible to flip/rotate/invert OWW in the Editor (transformed OWWs are not supported in RetroLemmini yet)\n");
 
+            WriteBoldText(richTextBox, "\n• Hotkeys\n");
+            richTextBox.AppendText(" • Removed support for Classic hotkeys (not relevant in this version of the Editor).\n");
+
             WriteBoldText(richTextBox, $"\n\n================ Previous Bugfixes ================\n");
 
             WriteBoldText(richTextBox, "\n• Cleanse Levels\n");
@@ -178,6 +191,10 @@ namespace RLEditor
             richTextBox.AppendText(" • Bugfix - .ini/.rlv is written in correctly when auto-updating levelpack.ini\n");
 
             WriteBoldText(richTextBox, "\n• Bugfixes\n");
+            richTextBox.AppendText(" • The selection rectangle is now drawn around the full object for OWWs.\n");
+            richTextBox.AppendText(" • Piece selection is now preserved on Undo/Redo.\n");
+            richTextBox.AppendText(" • Dropdown lists now show more items when expanded.\n");
+            richTextBox.AppendText(" • Grid is now drawn to its own layer.\n");
             richTextBox.AppendText(" • Whitespace and trailing backslashes are auto-trimmed from level titles.\n");
             richTextBox.AppendText(" • Any opaque pixel in a trigger mask is seen as a trigger area (it no longer has to be a specific shade of pink).\n");
             richTextBox.AppendText(" • 'Save As Image' sanitizes invalid characters when saving\n");
